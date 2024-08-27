f9374dd5f4735d22a89a9ef9bd7a7ca70712fbe0
Allene Norton <norton.allene@gmail.com>
2024-08-27 16:57:31 -0400

    select component separated and select working for customField

--- src/app/features/MassMessage.tsx
+++ src/app/features/MassMessage.tsx
@@ -1,24 +1,53 @@
+'use client'
+import { ComponentProps, useState } from "react";
 import { MultiSelectFields } from "@/app/types";
-
-
-
+import { Select, MenuItem } from "@mui/material"; // Assuming you're using Material-UI's Select component
 
 type Props = {
   fields: MultiSelectFields[]
 };
 
-export const MassMessage = ({fields}: Props)=> {
+export const MassMessage = ({ fields }: Props) => {
+  const [values, setValues] = useState({
+    customField: ""
+  });
+
+  const handleChangeValues: ComponentProps<typeof Select>["onChange"] = (
+    event
+  ) => {
+    const field = event.target.name;
+    setValues({
+      ...values,
+      [field]: event.target.value,
+    });
+  };
+
+  console.log(values)
+
   return (
     <div>
       <h2>MM</h2>
-      <select defaultValue={""}>
-      <option disabled value="" >Select a custom field:</option>
-      {fields.map(option => (
-        <option key={option.id} value={option.id}>
-          {option.name}
-        </option>
-      ))}
-    </select>
+      <Select
+        value={values.customField}
+        onChange={handleChangeValues}
+        name="customField"
+        displayEmpty
+        renderValue={(selected) => {
+          const selectedField = fields.find(
+            (field) => field.id === selected
+          );
+          if (!selected) {
+            return <div>Select a custom field</div>;
+          }
+          return selectedField?.name || "Select a custom field";
+        }}
+      >
+        {fields.map(option => (
+          <MenuItem key={option.id} value={option.id}>
+            {option.name}
+          </MenuItem>
+        ))}
+      </Select>
     </div>
-  )
+  );
 }
