76d361c4cbd7c83b84b431f8abbcdf7bf701ded2
Allene Norton <norton.allene@gmail.com>
2024-08-30 17:13:58 -0400

    mess with csp

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,18 +9,20 @@ export function middleware(request: NextRequest) {
   // If you have a custom domain add it below to the
   // space separated frame-ancestors list.
   const cspHeader = `
-    default-src 'self';
+    frame-ancestors https://dashboard.copilot.com/ https://*.copilot.app/;
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+
+/* default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
-    form-action 'self';
-    frame-ancestors https://dashboard.copilot.com/ https://*.copilot.app/;
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-`;
-//     style-src 'self' 'nonce-${nonce}';
+    form-action 'self'; 
+    
+    */
 
   // Replace newline characters and spaces
   const contentSecurityPolicyHeaderValue = cspHeader
